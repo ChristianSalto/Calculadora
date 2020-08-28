d0f4b0b3394601427bc1fd821ee89fb0ca25aef0
christian <romeosaltovk@gmail.com>
2020-08-29 00:35:05 +0200

    Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
-import React from 'react';
-import Button from './components/Button';
-import MathOperations from './components/MathOperations'
+import React, { useState } from 'react';
+import Functions from './components/Functions';
+import MathOperations from './components/MathOperations';
+import Numbers from './components/Numbers';
 import Result from './components/Result';
 import './App.css';
 
 
 const App = () => {
-
-    const clickHandler = text => {
-        console.log("Button.clickHandler", text)
-    };
+    const [stack, setStack] = useState("");
 
     console.log('Renderizado del componente App');
-    const myButtons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     return (
         <main className="react-calculator">
-            <Result value={"0"} />
-            <div className="numbers">
-                {myButtons.map(i => {
-                    return (
-                        <Button text={i} clickHandler={clickHandler} key={i} />
-                    )
-                })}
-                {/* <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>0</button> */}
-            </div>
-            <div className="functions">
-                <button>
-                    clear
-                </button>
-                <button>
-                    r
-                </button>
-            </div>
+            <Result value={stack} />
+            <Numbers onClickNumber={number => {
+                console.log("Click en el numero", number)
+                setStack(number)
+            }} />
+            <Functions
+                onContentClear={() => console.log("Content Clear...")}
+                onDelete={() => console.log("onDelete....")}
+            />
             <MathOperations
                 onClickOperation={operation =>
                     console.log("Operation:", operation)
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Button.css';
 
 const Button = ({ type, text, clickHandler }) => (
     <button className={type} onClick={() => clickHandler(text)}>
@@ -12,6 +13,6 @@ Button.propTypes = {
     type: PropTypes.string,
     text: PropTypes.string.isRequired,
     clickHandler: PropTypes.func.isRequired
-}
+};
 
 export default Button;
